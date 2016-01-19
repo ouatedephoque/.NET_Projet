33b48af0662d993e395f2410b1279d7bc27ab0f9
Jeshon Assunçao <jeshon.assuncao@he-arc.ch>
2016-01-19 14:31:59 +0100

    add Touche.cs + img

--- MIDI_Player/MIDI_Player/MainWindow.xaml.cs
+++ MIDI_Player/MIDI_Player/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Media;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Timers;
+using System.Windows.Threading;
 
 namespace MIDI_Player
 {
@@ -20,9 +16,125 @@ namespace MIDI_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string filename;
+        private bool isPlaying;
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            filename = null;
+            isPlaying = false;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0); // Every seconds 
+            timer.Tick += new EventHandler(timer_Elapsed);
+        }
+
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            //Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "midi files (*.mid)|*.mid";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                filename = openFileDialog.FileName;
+                labelFile.Content = filename;
+
+                InitMediaElm();
+            }
+            else
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: ");
+            }
+        }
+
+        private void InitMediaElm()
+        {
+            mediaElement.Source = new Uri(filename);
+
+            mediaElement.MediaOpened += new System.Windows.RoutedEventHandler(media_MediaOpened);
+            mediaElement.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
+            mediaElement.UnloadedBehavior = System.Windows.Controls.MediaState.Manual;
+        }
+
+        private void media_MediaOpened(object sender, System.Windows.RoutedEventArgs e)
+        {
+            TimeSpan tsDuration = mediaElement.NaturalDuration.TimeSpan;
+            slider.Maximum = (int)tsDuration.TotalSeconds;
+            string str = tsDuration.ToString(@"hh\:mm\:ss");
+            labelTimeStop.Content = str;
+        }
+
+        private void timer_Elapsed(object sender, EventArgs e)
+        {
+            Debug.WriteLine(mediaElement.Position.TotalSeconds);
+            slider.Value = mediaElement.Position.TotalSeconds;
+
+            TimeSpan tsTimeActu = TimeSpan.FromSeconds(slider.Value);
+            string str = tsTimeActu.ToString(@"hh\:mm\:ss");
+            labelTimeActual.Content = str;
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (filename != null)
+            {
+                if(isPlaying)
+                {
+                    mediaElement.Pause();
+                    timer.Stop();
+                }
+                else
+                {
+                   /*byte[] MIDI = File.ReadAllBytes(filename);
+                    for (int i = 0; i < MIDI.Length; i++)
+                    {
+                        if (MIDI[i] == 144)
+                        {
+                            Debug.WriteLine(MIDI[i + 1]);  //This is the note value
+                        }
+                    }*/
+
+                    mediaElement.Play();
+                    timer.Start();
+                }
+
+                isPlaying = !isPlaying;
+            }
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (filename != null)
+            {
+                mediaElement.Stop();
+                timer.Stop();
+                slider.Value = 0;
+
+                TimeSpan tsTimeActu = TimeSpan.FromSeconds(slider.Value);
+                string str = tsTimeActu.ToString(@"hh\:mm\:ss");
+                labelTimeActual.Content = str;
+
+                isPlaying = false;
+            }
         }
     }
 }
---
+++ MIDI_Player/MIDI_Player/Touche.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MIDI_Player
+{
+    class Touche
+    {
+        private double x;
+        private double y;
+        private Color color;
+        private bool isActived;
+
+        public Touche(double x, double y, Color color, bool isActived=false)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            this.isActived = isActived;
+
+            draw();
+        }
+
+        public bool actived()
+        {
+            return this.isActived;
+        }
+
+        public void setActived(bool actived)
+        {
+            this.isActived = actived;
+        }
+
+        private void draw()
+        {
+            /*Line myLine = new Line();
+            myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+            myLine.X1 = 1;
+            myLine.X2 = 50;
+            myLine.Y1 = 1;
+            myLine.Y2 = 50;
+            myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            myLine.StrokeThickness = 2;
+            gameArea.Children.Add(myLine);*/
+        }
+    }
+}
